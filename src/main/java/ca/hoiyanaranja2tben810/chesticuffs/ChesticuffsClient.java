bef03fdb158ac9f6703d7c915d833ce438efd0c9
HoiyaNaranja <137227369+HoiyaNaranja@users.noreply.github.com>
2025-07-07 23:28:55 -0400

    Initial commit

---
+++ src/main/java/ca/hoiyanaranja2tben810/chesticuffs/ChesticuffsClient.java
@@ -0,0 +1,10 @@
+package ca.hoiyanaranja2tben810.chesticuffs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChesticuffsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ca/hoiyanaranja2tben810/chesticuffs/ChesticuffsTCGDataGenerator.java
@@ -0,0 +1,11 @@
+package ca.hoiyanaranja2tben810.chesticuffs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChesticuffsTCGDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
